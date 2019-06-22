7df8d4a70f4c70d68d09055d220c20479f01757f
Pedro Pinheiro <pinheiro.pedrohl@gmail.com>
2019-06-22 15:57:27 -0300

    improvements(findAll): Integrate find all function with the AWS DynamoDB

--- findAll/main.go
+++ findAll/main.go
@@ -1,48 +1,53 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"net/http"
+	"os"
 )
 
-var movies = []struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-} {
-	{
-		ID: 1,
-		Name: "Avengers",
-	},
-	{
-		ID: 2,
-		Name: "Ant-Man",
-	},
-	{
-		ID: 3,
-		Name: "Thor",
-	},
-	{
-		ID: 4,
-		Name: "Hulk",
-	},
-	{
-		ID: 5,
-		Name: "Doctor Strange",
-	},
-}
-
 func findAll() (events.APIGatewayProxyResponse, error) {
-	response, err := json.Marshal(movies)
+	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body: "Error while retrieving AWS credentials",
+		},
+		nil
+	}
+
+	svc := dynamodb.New(cfg)
+	req := svc.ScanRequest(&dynamodb.ScanInput{
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+	})
+	res, err := req.Send(context.Background())
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body: "Error while scaning DynamoDB",
+		},
+		nil
+	}
+
+	response, err := json.Marshal(res.Items)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body: "Error while decoding to string value",
+		},
+		nil
 	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Headers: map[string]string{
-			"Content-Type": "application/json",
+			"Content-type": "application/json",
 		},
 		Body: string(response),
 	},
