bfde8c18c78f73f1e15d9a26b3cbc51504177ba9
Pedro Pinheiro <pinheiro.pedrohl@gmail.com>
2021-01-14 12:52:49 -0300

    commit

---
+++ deleteMovie/go.mod
@@ -0,0 +1,9 @@
+module deleteMovie
+
+go 1.14
+
+require (
+	github.com/aws/aws-lambda-go v1.18.0
+	github.com/aws/aws-sdk-go v1.33.13
+	github.com/aws/aws-sdk-go-v2 v0.24.0
+)
--- deleteMovie/main.go
+++ deleteMovie/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go/aws"
-	"net/http"
-	"os"
 )
 
 type Movie struct {
-	ID string `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -22,19 +23,19 @@ func deleteMovie(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 
 	if err := json.Unmarshal([]byte(request.Body), &movie); err != nil {
 		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusBadRequest,
-			Body: "Invalid Payload",
-		},
-		nil
+				StatusCode: http.StatusBadRequest,
+				Body:       "Invalid Payload",
+			},
+			nil
 	}
 
 	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body: "Error while retrieving AWS credentials",
-		},
-		nil
+				StatusCode: http.StatusInternalServerError,
+				Body:       "Error while retrieving AWS credentials",
+			},
+			nil
 	}
 
 	svc := dynamodb.New(cfg)
@@ -49,19 +50,19 @@ func deleteMovie(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 
 	if _, err := req.Send(context.Background()); err != nil {
 		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body: "Error while deleting movie from DynamoDB",
-		},
-		nil
+				StatusCode: http.StatusInternalServerError,
+				Body:       "Error while deleting movie from DynamoDB",
+			},
+			nil
 	}
 
 	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Headers: map[string]string{
-			"Content-Type": "application/json",
+			StatusCode: http.StatusOK,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
 		},
-	},
-	nil
+		nil
 }
 
 func main() {
